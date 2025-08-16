2bbb8144114e42ef95fecf1e0c469473ce91b650
kakanyero <kakanyero142@gmail.com>
2025-08-16 09:44:20 +0300

    week 3 assignment

---
+++ week_3.py
@@ -0,0 +1,17 @@
+
+#questiin 1
+
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:                      discount = price * (discount_percent / 100)                                             return price - discount                 return price
+
+
+#question 2
+
+price=float(input("Enter price: "))
+discount_percent=float(input("Enter discount percent: "))
+def calculate_discount(price, discount_percent):
+    if discount_percent >= 20:
+        discount = price * (discount_percent / 100)
+        return price - discount
+    return price
+print(calculate_discount(price,discount_percent))
